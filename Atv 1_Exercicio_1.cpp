ae83b0c4ce8bc7f9ebf6ac54c24b8dcfe18e6848
Jú Gabriele <127549501+Juli-Gabriele@users.noreply.github.com>
2026-02-15 17:48:34 -0300

    Create Atv 1_Exercicio_1.cpp

---
+++ Atv 1_Exercicio_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+ int main(){
+    double V[10]{7.5,4.0,9.3,5.5,8.0,6.5,3.2,10.0,5.8};
+    double soma =0, media= 0, maior =0;
+    int i, q = 0; // q = quantidade
+    for (i =0 ; i < 10; i++){
+        soma = V[i] + soma;
+    }
+     media = soma/ 10;
+
+     for (i =0 ; i < 10; i++){
+        if (V[i] > media){
+           q = q+ 1;
+        }
+    }
+    for (i =0 ; i < 10; i++){
+       double aux = V[i];
+       if (aux > maior){
+        maior = aux;
+       }
+    }
+
+    cout << "A soma e:" << soma <<endl;
+    cout << "A media das notas: "<< media <<endl;
+     cout <<"A quantidade de numeros acima da media e: "<< q <<endl;
+    cout << "O maior numero e :"<< maior <<endl;
