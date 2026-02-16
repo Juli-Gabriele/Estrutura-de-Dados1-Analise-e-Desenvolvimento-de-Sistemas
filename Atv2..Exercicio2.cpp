d4743596b48779bd122ef80d9e00de2030a4abb3
Jú Gabriele <127549501+Juli-Gabriele@users.noreply.github.com>
2026-02-16 20:13:31 -0300

    Create Atv2..Exercicio2.cpp

---
+++ Atv2..Exercicio2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define tam 3
+  using namespace std;
+int main()
+{
+    int v[tam],i,j;
+    bool  repetido = false;
+    
+     for(i = 0; i < tam ; i++){
+         cin >> v[i];
+     }
+     for(i = 0; i < tam - 1 ; i++){
+          
+         for(j = i + 1; j < tam  ; j++){
+                
+                if (v[i] == v[j]){
+                        repetido = true;
+                 }
+                }     
+                 
+     }
+     
+     if (repetido != true){
+         cout << "ok"<< endl;
+     }else{
+         cout <<"no"<< endl;
+     }
